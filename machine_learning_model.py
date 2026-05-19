605cccbccfe4f0e2e422421d80af711b6f7de2d8
ivjoli <ivjoli@ucsc.edu>
2026-05-19 12:07:00 -0700

    outline for code

---
+++ machine_learning_model.py
@@ -0,0 +1,75 @@
+"""
+imports here
+"""
+
+
+
+
+"""
+load data + data cleaning 
+"""
+
+
+
+
+
+
+"""
+Data Splicing / SCaling
+Train 70%
+Validation 15%
+Test 15%
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Model + Dataloader Class
+"""
+
+# Model class
+
+
+
+
+
+# Dataloader Class
+
+
+"""
+Batching Inputs
+"""
+# Batch train inputs/outputs
+# Batch validation inputs/outputs
+# batch test inputs/outputs
+
+
+
+"""
+Optimizer + Loss Function
+"""
+# optimizer = Adam with learning rate 0.01
+# Loss Function = MSE
+
+
+"""
+Training Loop
+"""
+# Epoch == ??
+
+"""
+Testing Loop
+"""
+
+"""
+Class Identifier + Output
+"""
