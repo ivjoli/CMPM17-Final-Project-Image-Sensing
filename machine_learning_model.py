a59d4feaa5aa7462ff623b245ba90827bd95a490
ivjoli <ivjoli@ucsc.edu>
2026-06-04 18:02:01 -0700

    batch norm, but loss stayed the same

--- machine_learning_model.py
+++ machine_learning_model.py
@@ -159,24 +159,30 @@ Model + Dataloader Class
 class myNN(nn.Module):
     def __init__(self):
         super().__init__() # this calls a pytorch function to do math so no need to indent
-        self.layer1 = nn.Conv2d(3, 40,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 1
-        self.layer2 = nn.Conv2d(40, 80,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 2
-        self.layer3 = nn.Conv2d(80, 50,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 2
-        self.layer4 = nn.Conv2d(50, 100,  3, 1 , 1) #The output can be modified for loss testing
         
+        self.layer1 = nn.Conv2d(3, 40,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 1
+        self.batchNorm1 = nn.BatchNorm2d( 40 )
+        self.layer2 = nn.Conv2d(40, 80,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 2
+        self.batchNorm2 = nn.BatchNorm2d( 80 )
+        self.layer3 = nn.Conv2d(80, 50,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 2
+        self.batchNorm3 = nn.BatchNorm2d( 50 )
+        self.layer4 = nn.Conv2d(50, 100,  3, 1 , 1) #The output can be modified for loss testing
+        self.batchNorm4 = nn.BatchNorm2d( 100 )
+
         self.pool = nn.MaxPool2d(kernel_size=2, stride= 2)
         self.fc1 = nn.Linear(14 * 14 * 100, 400)
         self.fc2 = nn.Linear(400,1)
         self.relu = nn.ReLU() # activation function
     #Forward Pass for Milestone 2!
     def forward(self, inputs):
-        partial = self.relu(self.layer1(inputs))
+
+        partial = self.relu(self.batchNorm1(self.layer1(inputs)))
         partial = self.pool(partial)
-        partial = self.relu(self.layer2(partial))
+        partial = self.relu(self.batchNorm2(self.layer2(partial)))
         partial = self.pool(partial)
-        partial = self.relu(self.layer3(partial))
+        partial = self.relu(self.batchNorm3(self.layer3(partial)))
         partial = self.pool(partial)
-        partial = self.relu(self.layer4(partial))
+        partial = self.relu(self.batchNorm4(self.layer4(partial)))
         partial = self.pool(partial)
 
         partial = partial.flatten(start_dim=1)
