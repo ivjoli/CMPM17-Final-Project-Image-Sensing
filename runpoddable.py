166cc4335d0cbe24c8ed65cd76501e949126c85f
ivjoli <ivjoli@ucsc.edu>
2026-06-07 14:00:58 -0700

    changed some nodes and increased epoch

--- runpoddable.py
+++ runpoddable.py
@@ -166,13 +166,13 @@ class myNN(nn.Module):
     def __init__(self):
         super().__init__() # this calls a pytorch function to do math so no need to indent
         
-        self.layer1 = nn.Conv2d(3, 40,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 1
-        self.batchNorm1 = nn.BatchNorm2d( 40 )
-        self.layer2 = nn.Conv2d(40, 30,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 2
-        self.batchNorm2 = nn.BatchNorm2d( 30 )
-        self.layer3 = nn.Conv2d(30, 50,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 2
-        self.batchNorm3 = nn.BatchNorm2d( 50 )
-        self.layer4 = nn.Conv2d(50, 100,  3, 1 , 1) #The output can be modified for loss testing
+        self.layer1 = nn.Conv2d(3, 30,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 1
+        self.batchNorm1 = nn.BatchNorm2d( 30 )
+        self.layer2 = nn.Conv2d(30, 40,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 2
+        self.batchNorm2 = nn.BatchNorm2d( 40 )
+        self.layer3 = nn.Conv2d(40, 20,  kernel_size= 3, stride=1 , padding=1) # inputs to layer 2
+        self.batchNorm3 = nn.BatchNorm2d( 20 )
+        self.layer4 = nn.Conv2d(20, 100,  3, 1 , 1) #The output can be modified for loss testing
         self.batchNorm4 = nn.BatchNorm2d( 100 )
 
         self.pool = nn.MaxPool2d(kernel_size=2, stride= 2)
@@ -243,7 +243,7 @@ model = model.to(device)
 
 #Training Loop
 
-epoch = 30  #TEMPORARY FOR NOW WHILE WE wait for RUNPOD - runpod works
+epoch = 50  #TEMPORARY FOR NOW WHILE WE wait for RUNPOD - runpod works
 #counter to know how many batches = len(dataloader)
 for i in range(epoch):
     model.train() # sets the model into training mode, allows weights to be changed
